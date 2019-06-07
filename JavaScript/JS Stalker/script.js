803c015aa932d451fec0120d848e794f6e070023
Robert Elias <RobertElias@users.noreply.github.com>
2019-06-06 21:01:26 -0700

    Updates

---
+++ JavaScript/JS Stalker/script.js
@@ -0,0 +1,7 @@
+var firstName = prompt("What is your first name?");
+var lastName = prompt("What is your last name?");
+var Age = prompt("What is your age?");
+var fullName = firstName + " " + lastName
+console.log("Your full name is " + fullName);
+console.log("You are: " + Age + " years old!");
+

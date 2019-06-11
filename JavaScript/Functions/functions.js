c647167812c7767818580eee4cc34c3e87633c17
Robert Elias <RobertElias@users.noreply.github.com>
2019-06-10 18:44:57 -0700

    Function Updates

---
+++ JavaScript/Functions/functions.js
@@ -0,0 +1,14 @@
+function doSomething() {
+    console.log("Hello World!!");
+}
+
+function sayHi() {
+    console.log("Hello World!!");
+}
+
+function singSong() {
+    console.log("Twinkle");
+    console.log("Twinkle");
+    console.log("Little");
+    console.log("Star");
+}

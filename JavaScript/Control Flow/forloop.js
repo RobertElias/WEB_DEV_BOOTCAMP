f921262c7ef7b85f2d1883f74650cb289d42e0d0
Robert Elias <RobertElias@users.noreply.github.com>
2019-06-10 18:23:05 -0700

    Updates

--- JavaScript/Control Flow/forloop.js
+++ JavaScript/Control Flow/forloop.js
@@ -1,17 +1,27 @@
-for(var count = 0; count < 6; count++) {
-    console.log(count);
-}
+// for(var count = 0; count < 6; count++) {
+//     console.log(count);
+// }
 
-var count = 1;
+// var count = 1;
 
-while(count < 6) {
-    console.log("count is: " + count);
-    count++;
-}
+// while(count < 6) {
+//     console.log("count is: " + count);
+//     count++;
+// }
 
-var str = "hello";
+// var str = "hello";
 
-for(var i = 0; i , str.length; i++){
-    console.log(str[i]);
-}
+// for(var i = 0; i , str.length; i++){
+//     console.log(str[i]);
+// }
+
+// for(var i = 0; i < 16; i+=8) {
+//     console.log(i);
+// }
+
+// var str = "ahceclwlxo";
+
+// for(vari = 1; i < str.length; i+=2) {
+//     console.log(str[i]);
+// }
 

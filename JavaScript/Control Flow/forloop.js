0b1bb9702c4ad0383a6507e19d0194aab8945eae
Robert Elias <RobertElias@users.noreply.github.com>
2019-06-10 18:31:15 -0700

    UPDATES

--- JavaScript/Control Flow/forloop.js
+++ JavaScript/Control Flow/forloop.js
@@ -25,3 +25,36 @@
 //     console.log(str[i]);
 // }
 
+
+//Print all numbers between -10-19
+console.log("printing all numbers -10 to 19")
+for(var i = -10; i < 20; i++){
+    console.log(i);
+}
+
+//print all even between 10 and 40
+console.log("printing all even numbers from 10 to 40")
+for(var i = 10; i <= 40; i+=1){
+    if(i % 2 === 0){
+      console.log(i);
+    }
+}
+//print all odd between 300 and 333
+
+console.log("printing all odd numbers from 300 to 333")
+for(var i = 300; i <= 333; i++){
+    if(i % 2 !==0) {
+        console.log(i);
+    }
+}
+
+//Print all numbers divisible by 5 and 3 between 5 and 50!
+
+console.log("printing all numbers divisible by 5 and 3 between 5 and 50!")
+var counter = 5;
+
+for( var i =5; i <= 50;i++) {
+    if(i % 5 === 0 && i % 3 === 0) {
+        console.log(i);
+    }
+}

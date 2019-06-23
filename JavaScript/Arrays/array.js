7da9674353fc686161bfdfd28f5b0b607c522c57
Robert Elias <RobertElias@users.noreply.github.com>
2019-06-22 19:31:53 -0700

    Updates

---
+++ JavaScript/Arrays/array.js
@@ -0,0 +1,89 @@
+var friends = ["Charlie", "Liz", "David", "Mattias"];
+
+friends[4] = "Amelie";
+friends[0] = "Chuck";
+friends[1] = "Lizzie";
+
+
+var animals = ['pigs', 'goats', 'sheep'];
+
+console.log(animals.push('cows'));
+// expected output: 4
+
+console.log(animals);
+// expected output: Array ["pigs", "goats", "sheep", "cows"]
+
+animals.push('chickens');
+
+console.log(animals);
+// expected output: Array ["pigs", "goats", "sheep", "cows", "chickens"]
+
+
+///.pop
+
+var plants = ['broccoli', 'cauliflower', 'cabbage', 'kale', 'tomato'];
+
+console.log(plants.pop());
+// expected output: "tomato"
+
+console.log(plants);
+// expected output: Array ["broccoli", "cauliflower", "cabbage", "kale"]
+
+plants.pop();
+
+console.log(plants);
+// expected output: Array ["broccoli", "cauliflower", "cabbage"]
+
+//shift
+
+var array1 = [1, 2, 3];
+
+var firstElement = array1.shift();
+
+console.log(array1);
+// expected output: Array [2, 3]
+
+console.log(firstElement);
+// expected output: 1
+
+//unshift
+var array1 = [1, 2, 3];
+
+console.log(array1.unshift(4, 5));
+// expected output: 5
+
+console.log(array1);
+// expected output: Array [4, 5, 1, 2, 3]
+
+//IndexOf
+
+var beasts = ['ant', 'bison', 'camel', 'duck', 'bison'];
+
+console.log(beasts.indexOf('bison'));
+// expected output: 1
+
+// start from index 2
+console.log(beasts.indexOf('bison', 2));
+// expected output: 4
+
+console.log(beasts.indexOf('giraffe'));
+// expected output: -1
+
+//Slice
+var months = ['Jan', 'March', 'April', 'June'];
+months.splice(1, 0, 'Feb');
+// inserts at index 1
+console.log(months);
+// expected output: Array ['Jan', 'Feb', 'March', 'April', 'June']
+
+months.splice(4, 1, 'May');
+// replaces 1 element at index 4
+console.log(months);
+// expected output: Array ['Jan', 'Feb', 'March', 'April', 'May']
+
+var nums = [1,2,2, "a", "b", 445, 34]
+
+var letters = num.slice(3,5)
+
+
+

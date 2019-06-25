997c0b3f9d5a7202ad028a9f07e6f6f077ad9735
Robert Elias <RobertElias@users.noreply.github.com>
2019-06-24 20:12:47 -0700

    Updates

--- JavaScript/Arrays/iterate.js
+++ JavaScript/Arrays/iterate.js
@@ -14,3 +14,21 @@ colors.forEach(function(color){
    //color is placeholder, call it whatever you want
    console.log(color); 
 });
+
+var numbers = [1,2,3,4,5,6,7,8,9,10];
+
+numbers.forEach(function(color) {
+    if(color % 3 === 0) {
+        console.log(color);
+    }
+});
+
+[1,2,3].forEach(function(el, i, arr) {
+    console.log(el, i, arr);
+  });
+
+  function logNums(el, i, arr) {
+    console.log(el, i, arr);
+  }
+   
+  [1,2,3].forEach(logNums);
--- JavaScript/Arrays/todo.js
+++ JavaScript/Arrays/todo.js
@@ -1,20 +1,44 @@
 var todos = ["Buy New Turtle"];
-window.setTimeout(function() {
-var input = prompt("What would you like to do?");
+window.setTimeout(function () {
+    var input = prompt("What would you like to do?");
 
-while (input !== "quit") {
+    while (input !== "quit") {
+        //handle input
+        if (input === "list") {
+            listTodos();
+        } else if (input === "new") {
 
-    if (input === "list") {
-        console.log(todos);
-    } else if (input === "new") {
-        //ask for new todo
-        var newTodo = prompt("Enter new todo");
-        //add to todos array
-        todos.push(newTodo);
+        } else if (input === "delete") {
+
+
+        }
+        //ask again for new input
+        input = prompt("What would you like to do?");
     }
-    //ask again for new input
-    input = prompt("What would you like to do?");
-}
-console.log("OK, YOU QUIT THE APP");
+    console.log("OK, YOU QUIT THE APP");
 
 }, 500);
+
+function listTodos() {
+    console.log("**********")
+    todos.forEach(function (todo, i) {
+        console.log(i + ": " + todo);
+    });
+    console.log("**********")
+}
+
+function addTodo() {
+    //ask for new todo
+    var newTodo = prompt("Enter new todo");
+    //add to todos array
+    todos.push(newTodo);
+    console.log("Added Todo")
+}
+
+function deleteTodo() {
+    //ask for index of todo to be deleted
+    var index = prompt("Enter index of todo to delete");
+    //delete that todo item
+    todos.splice(index, 1);
+    console.log("Deleted Todo");
+}

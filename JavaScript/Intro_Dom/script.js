f5739abf357943c93c65db0fa50c9f609416632e
Robert Elias <RobertElias@users.noreply.github.com>
2019-06-28 21:57:15 -0700

    Updates

---
+++ JavaScript/Intro_Dom/script.js
@@ -0,0 +1,15 @@
+var h1 = document.querySelector("h1");
+
+h1.style.color = "pink";
+
+
+document.URL;
+document.head;
+document.body;
+document.links;
+
+document.getElementById()
+document.getElementsByClassName()
+document.getElementsByTagName()
+document.querySelector()
+document.querySelectorAll()
